8e1e857e66047363251ee6624412ca0607c412f2
AlessandroSampaio <alessandrosampaio521@gmail.com>
2023-11-19 16:29:52 -0300

    Atribui a renderização do board a uma lista de pedidos

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,10 +1,30 @@
+import { Order } from '../../types/Order';
 import OrdersBoard from '../OrdersBoard';
 import { Container } from './styles';
+
+const orders : Order[] = [
+  {
+    '_id': '655a5d14c46e4013ff08f39c',
+    'table': '1',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': '1700412044817-quatro-queijos.png',
+          'price': 40,
+        },
+        'quantity': 3,
+        '_id': '655a5d14c46e4013ff08f39d'
+      },
+    ],
+  }
+];
 
 export default function Orders() {
   return (
     <Container>
-      <OrdersBoard icon="⌚" title="Fila de espera" />
+      <OrdersBoard icon="⌚" title="Fila de espera" orders={orders}/>
       <OrdersBoard icon="🧑‍🍳" title="Em Produção"  />
       <OrdersBoard icon="🆗" title="Feito"  />
     </Container>
--- src/components/OrdersBoard/index.tsx
+++ src/components/OrdersBoard/index.tsx
@@ -1,27 +1,27 @@
+import { Order } from '../../types/Order';
 import { Board, OrdersContainer } from './styles';
 
 interface OrderBoardsProps {
   icon: string;
   title: string;
+  orders?: Array<Order>;
 }
 
-export default function OrdersBoard({ icon, title }: OrderBoardsProps) {
+export default function OrdersBoard({ icon, title, orders }: OrderBoardsProps) {
   return (
     <Board>
       <header>
         <span>{icon}</span>
         <strong>{title}</strong>
-        <span>1</span>
+        <span>{orders?.length ?? 0}</span>
       </header>
       <OrdersContainer>
-        <button type="button">
-          <strong>Mesa 2</strong>
-          <span>2</span>
-        </button>
-        <button type="button">
-          <strong>Mesa 2</strong>
-          <span>2</span>
-        </button>
+        {orders?.map((order) => (
+          <button type="button" key={order._id}>
+            <strong>Mesa {order.table}</strong>
+            <span>{order.products.length} iten(s)</span>
+          </button>
+        ))}
       </OrdersContainer>
     </Board>
   );
